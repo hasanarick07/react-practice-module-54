379624e6871eabd7d93f0052dd88b019b4f05c33
Arick Hasan <hasanarick000@gmail.com>
2022-04-03 19:29:45 +0600

    first1

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,25 @@
 import React from "react";
 
-const Cart = () => {
+const Cart = ({ cart, removeFromCart }) => {
   return (
     <div>
-      <h1>cart</h1>
+      <h1>Items Added : {cart.length}</h1>
+      {cart.map(cart => (
+        <div className="flex h-10 items-center mt-3 content-between">
+          <img
+            className="mr-3 rounded-full h-10 p-1 w-auto"
+            src={cart.picture}
+            alt=""
+          />
+          <p className="mr-5">{cart.name}</p>
+          <button
+            onClick={() => removeFromCart(cart)}
+            className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-1.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+          >
+            X
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import useShirts from "../../hooks/useShirts";
 import Card from "../Card/Card";
 import Cart from "../Cart/Cart";
 
 const Home = () => {
   const [tshirts, setTshirts] = useShirts();
+  const [cart, setCart] = useState([]);
+  const addToCart = add => {
+    const exist = cart.find(cart => cart._id === add._id);
+    if (!exist) {
+      const newCart = [...cart, add];
+      setCart(newCart);
+    } else {
+      alert("Can not add a product twice");
+    }
+  };
+  const removeFromCart = add => {
+    const remain = cart.filter(cart => cart._id !== add._id);
+    setCart(remain);
+  };
   return (
     <div className="flex flex-row">
       <div className="basis-4/5 grid grid-cols-3 gap-4">
         {tshirts.map(shirt => (
-          <Card key={tshirts._id} shirt={shirt}></Card>
+          <Card key={tshirts._id} addToCart={addToCart} shirt={shirt}></Card>
         ))}
       </div>
       <div className="basis-1/5">
-        <Cart></Cart>
+        <Cart cart={cart} key={cart._id} removeFromCart={removeFromCart}></Cart>
       </div>
     </div>
   );
